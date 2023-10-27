4c1e5fac45a7a9011311886bd1ef15f139272017
RisingSatalite <107381820+RisingSatalite@users.noreply.github.com>
2023-10-27 15:02:17 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+//module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+};
